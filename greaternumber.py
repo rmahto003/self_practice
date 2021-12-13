19a9e2f011e6a7a5502a3baca58afd71903c13d0
rmahto003 <ranjeetkrmahto3@gmail.com>
2021-12-13 10:50:30 +0530

    Self Commitng

---
+++ greaternumber.py
@@ -0,0 +1,8 @@
+a = int(input("Please Enter Number = "))
+b = int(input("Please Enter Number = "))
+
+if a > b :
+    print(str(a) + " is greater than " + str(b))
+
+else :
+    print(str(a) + " is smaller than " + str(b))
