2e5afb4d0243cc2c1fee01e4822024699705b2b3
Barun Ghosh <barungh@hotmail.com>
2022-01-22 17:39:41 +0530

    Function for searching state ☂

--- capital.py
+++ capital.py
@@ -26,21 +26,33 @@ states_capitals = { 'Andra Pradesh'     : 'Amaravati' ,
                     'Tripura'           : 'Agartala' ,
                     'Uttar Pradesh'     : 'Lucknow' ,
                     'Uttarakhand'       : 'Dehradun' ,
-                    'West Bengal'       : 'Kolkata'    }
+                    'West Bengal'       : 'Kolkata'    
+                    }
 
 
+print("What do you want to do ?")
+option = int(input("Search capital (press 1) \nSearch State (Press 2) \nEnter your choice : "))
 
-d =input('Enter State : ').title()
+if option == 1:
+    d =input('Enter State : ')
+    if d.title() in states_capitals:
+        print('capital is : ', states_capitals[d])
+    else:
+        print('plese enter correct state')
+        
+if option == 2:
+    city = input("Enter a city name : ")
 
-if d in states_capitals:
-    print('capital is : ', states_capitals[d])
-else:
-    print('plese enter correct state')
+    result = ''
 
+    for k,v in states_capitals.items():
+        if v.lower() == city.lower():
+            result = k
+            break
+        else:
+            result = ''
 
-
-
-
-
-
-
+    if len(result)==0:
+        print(city, " is not in the list of State Capitals")
+    else:
+        print(city.title(), " is capital of ", result)
